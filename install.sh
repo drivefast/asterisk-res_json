dac83ca4b0dd60bd35dcde19c1ff0beb26aa4056
Radu Maierean <radu.maierean@gmail.com>
2011-03-14 00:01:06 -0700

    added trivialinstall script

---
+++ install.sh
@@ -0,0 +1,7 @@
+if [ -s include/asterisk.h ] ; then
+	echo "please cd into the directory where the asterisk source has been untarred\n"
+	exit
+fi
+cp asterisk-res_json/res_json.c res/.
+cp asterisk-res_json/cJSON.h include/asterisk/.
+cp asterisk-res_json/cJSON.c main/.
