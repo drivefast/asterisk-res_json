fd261a653da07803ab6042849cc7557c09b57cac
Radu Maierean <radu.maierean@gmail.com>
2013-05-26 17:42:02 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 if [ ! -s include/asterisk.h ] ; then
-	echo "please cd into the directory where the asterisk source has been untarred\n"
+	echo "please cd into the directory where the asterisk source has been untarred"
 	exit
 fi
 cp asterisk-res_json/res_json.c addons/.
 cp asterisk-res_json/cJSON.h include/asterisk/.
 cp asterisk-res_json/cJSON.c addons/.
-echo "edit addons/Makefile: add res_json to the list of modules built, and\n"
-echo "    res_json.so: cJSON.o res_json.o\n"
+echo "edit addons/Makefile: add res_json to the list of modules built, and"
+echo "    res_json.so: cJSON.o res_json.o"
